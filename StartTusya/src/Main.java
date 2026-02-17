e870dc65d0acc85dd71f6bbf6b26704ad934f78e
qweenkefi <dumbtao@gmail.com>
2026-02-17 15:35:53 +0300

    Add files via upload

---
+++ StartTusya/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 250;
+        int z = 50;
+        System.out.println(x + y);
+        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("hello " + name);
+
+    }
+}
